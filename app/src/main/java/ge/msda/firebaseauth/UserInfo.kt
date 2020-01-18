6cf9b0eff4d7bc0bbeab3e6cb52580ff4853c331
Saba <46112197+Elmo33@users.noreply.github.com>
2020-01-18 12:45:03 +0400

    Update UserInfo.kt

--- app/src/main/java/ge/msda/firebaseauth/UserInfo.kt
+++ app/src/main/java/ge/msda/firebaseauth/UserInfo.kt
@@ -4,8 +4,8 @@ import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class UserInfo(
-    val name: String = "triko",
-    val mobile: String? = "shmiko",
-    val address: String? = "niko",
-    val link: String? = "diko"
+    val name: String = "",
+    val mobile: String? = "",
+    val address: String? = "",
+    val link: String? = ""
 )
