3ccbd72759af2d2172ac9d13264c9149e1cd515c
Saba <46112197+Elmo33@users.noreply.github.com>
2020-01-18 10:36:31 +0400

    Update LoginActivity.kt

--- app/src/main/java/ge/msda/firebaseauth/LoginActivity.kt
+++ app/src/main/java/ge/msda/firebaseauth/LoginActivity.kt
@@ -11,26 +11,40 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
+    // ვქმნით auth object-ს და ვანიჭებთ მას FirebaseAuth-ის ტიპს
+
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // ვაკავშირებთ auth-ს Firebase-სთან
+        
         auth = FirebaseAuth.getInstance()
 
+        
+        // login button ისთვის განკუთვნილი კოდი
+        
         SignIn.setOnClickListener {
+            
+            // იღებს მომხმარებლის მიერ შეყვანილ იმეილს და პაროლს
 
             val email: String = EmailSignIn.text.toString()
             val password: String = PasswordSignIn.text.toString()
 
+            // ამოწმებს არის თუ არა რომელიმე ხაზი ცარიელა
+            
             if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
 
                 Toast.makeText(this@LoginActivity, "Please fill all the fields", Toast.LENGTH_LONG)
                     .show()
 
             } else {
-
+                
+                // ვაწვდით ემაილს და პაროლს signInWithEmailAndPassword ფუნქციას რომლის წარმატებით
+                // შესრულების შემთხვევაშიც გამოგვაქვს Toast
+                
                 auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, OnCompleteListener { task ->
                         if (task.isSuccessful) {
@@ -46,11 +60,15 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
+        // რეგისტრაციისთვის განკუთვნილი ღილაკი
+        
         notmember.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             finish()
         }
+        
+        // პაროლის შეხსენებისთვის განკუთვნილი ღილაკი
 
         Forgot.setOnClickListener {
             val intent = Intent(this, ForgotPasswordActivity::class.java)
